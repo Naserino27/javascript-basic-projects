e1d0c15107115e9aebb225e717670f5e4be96539
Naserino27 <nasereddin.sahhar@elev.ga.ntig.se>
2021-03-24 12:04:15 +0100

    first commit

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "yellow"];
+//sätt upp globala variabler
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//lyssna efter knappen
+btn.addEventListener('click', function () {
+    //få fram ett slumpat tal mellan 0-3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    //byt färg på sidan och texten beroende på vilket nummer i arrayen som slumpas
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+//Sätt upp en funktion som genererar ett slumpat nummer mellan 0-3
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//globala variabler
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//lyssna efter knappen och gör en loop som skapar en slumpad hexfärg
+btn.addEventListener("click", function () {
+    //variabel som håller # som sedan fylls på med 6 slumpade hexvärden
+    let hexColor = "#";
+
+    //loopa 6ggr med ett nytt hexvärde varje gång
+    for (let i = 0; i < 6; i++) {
+        //lägg på nytt hexvärde för varje loop
+        hexColor += hex[getRandomNumber()];
+    }
+
+    //byt bakgrundsfärgen till det värdet vi får fram
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
